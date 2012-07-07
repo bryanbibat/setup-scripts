b2f9a7dfc7c6bd4c114daee28e6e408abd651d0c
Bryan Bibat <bry@bryanbibat.net>
2012-07-07 19:47:00 +0800

    minor changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Bry's setup scripts
 
-Scripts for setting up new development machines
+Scripts for setting up new development machines. Parts shamelessly ripped from [railsready.sh](https://github.com/joshfng/railsready).
 
     wget --no-check-certificate https://raw.github.com/bryanbibat/setup-scripts/master/ubuntu1204.sh && bash ubuntu1204.sh
--- ubuntu1204.sh
+++ ubuntu1204.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+shopt -s nocaseglob
+set -e
 
 # switch to faster server (ph.archive.ubuntu.com is in UK)
-echo -e "\nUpdating your system...\n"
+echo -e "\n=> Updating your system...\n"
 sudo sed -i.bak 's/ph.archive.ubuntu.com/ftp.tsukuba.wide.ad.jp\/Linux/' /etc/apt/sources.list
 
 # update system
@@ -9,28 +11,28 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # install build tools, rvm/ruby dependencies, and other basic stuff (git, Inconsolata)
-echo -e "\nInstalling basic packages...\n"
+echo -e "\n=> Installing basic packages...\n"
 sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion ttf-inconsolata
 
 # install RVM/Ruby
-echo -e "\nInstalling RVM and Ruby 1.9.3...\n"
+echo -e "\n=> Installing RVM and Ruby 1.9.3...\n"
 curl -L https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install 1.9.3
 rvm use 1.9.3 --default
 
 # install gVim
-echo -e "\nInstalling gVim dependencies...\n"
+echo -e "\n=> Installing gVim dependencies...\n"
 sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev mercurial
-echo -e "\nDownloading gVim source...\n"
+echo -e "\n=> Downloading gVim source...\n"
 hg clone https://code.google.com/p/vim/ /tmp/vim
-echo -e "\nBuilding gVim...\n"
+echo -e "\n=> Building gVim...\n"
 cd /tmp/vim/src/
 make distclean
 ./configure --with-features=huge --enable-gui=gnome2 --enable-rubyinterp 
 make
 sudo make install
-echo -e "\nSyncing gVim...\n"
+echo -e "\n=> Syncing gVim...\n"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 wget https://raw.github.com/bryanbibat/setup-scripts/master/.vimrc -O ~/.vimrc
 gvim +BundleInstall +qall
@@ -39,11 +41,11 @@ ruby extconf.rb
 make
 
 # install Node.js
-echo -e "\nDownloading Node.js v8.1...\n"
+echo -e "\n=> Downloading Node.js v8.1...\n"
 cd /tmp
 wget http://nodejs.org/dist/v0.8.1/node-v0.8.1.tar.gz
 tar zxvf node-v0.8.1.tar.gz
-echo -e "\nBuilding Node.js v8.1...\n"
+echo -e "\n=> Building Node.js v8.1...\n"
 cd node-v0.8.1.tar.gz
 ./configure
 make
