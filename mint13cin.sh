edf132f18f71d5302b87b86e606535909acfb2c8
Bryan Bibat <bry@bryanbibat.net>
2012-07-28 18:46:51 +0800

    Add Mint 13 cinnamon

---
+++ mint13cin.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+shopt -q login_shell
+if [ $? = "1" ]; then
+  echo "Not a login shell. See https://rvm.io/integration/gnome-terminal/ to enable it."
+  exit 1
+fi
+
+shopt -s nocaseglob
+
+# switch to faster server (ph.archive.ubuntu.com is in UK)
+echo -e "\n=> Updating your system...\n"
+sudo sed -i.bak 's/packages.linuxmint.com/mirror.nus.edu.sg\/LinuxMint/' /etc/apt/sources.list
+
+# update system
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install build tools, rvm/ruby dependencies, and other basic stuff (git, Inconsolata)
+echo -e "\n=> Installing basic packages...\n"
+sudo apt-get -y install build-essential libreadline6-dev curl git-core zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libncurses5-dev automake libtool bison subversion ttf-inconsolata gitk
+
+# install RVM/Ruby
+echo -e "\n=> Installing RVM and Ruby 1.9.3...\n"
+curl -L https://get.rvm.io | bash -s stable --ruby
+source ~/.rvm/scripts/rvm
+
+# install gVim
+echo -e "\n=> Installing gVim dependencies...\n"
+sudo apt-get -y install libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libxpm-dev libxt-dev mercurial
+echo -e "\n=> Downloading gVim source...\n"
+hg clone https://code.google.com/p/vim/ /tmp/vim
+echo -e "\n=> Building gVim...\n"
+cd /tmp/vim/src/
+make distclean
+./configure --with-features=huge --enable-gui=gnome2 --enable-rubyinterp 
+make
+sudo make install
+echo -e "\n=> Syncing gVim...\n"
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+wget https://raw.github.com/bryanbibat/setup-scripts/master/.vundle -O ~/.vimrc
+vim +BundleInstall +qall
+cd ~/.vim/bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
+wget https://raw.github.com/bryanbibat/setup-scripts/master/.vimrc -O ~/.vimrc
+
+# install Node.js
+echo -e "\n=> Downloading Node.js v8.1...\n"
+cd /tmp
+wget http://nodejs.org/dist/v0.8.1/node-v0.8.1.tar.gz
+tar zxvf node-v0.8.1.tar.gz
+echo -e "\n=> Building Node.js v8.1...\n"
+cd node-v0.8.1
+./configure
+make
+sudo make install
+
+cd ~
+
+# setup Git
+echo -e "\n=> Setting up git...\n"
+git config --global user.name "Bryan Bibat"
+git config --global user.email "bry@bryanbibat.net"
+wget https://raw.github.com/bryanbibat/setup-scripts/master/.gitignore_global -O ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
+ssh-keygen -t rsa -C "bry@bryanbibat.net"
+
+echo -e "\n=> Installing system monitor indicator...\n"
+sudo apt-get -y install indicator-multiload
+
+echo -e "\n=> Initial setup complete!\n"
+echo -e "\n=> Final steps:\n   * Install VM guest additions\n   * Update font for terminal\n   * Enable system indicator\n"
